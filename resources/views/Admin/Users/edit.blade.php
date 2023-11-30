48398a9f49bddbfa60b4d1f11ffc22a36857ebc5
thakurakhilesh1998 <thakurakhilesh20@gmail.com>
2023-11-30 12:31:15 +0530

    Admin Login work partially completed

--- resources/views/Admin/Users/edit.blade.php
+++ resources/views/Admin/Users/edit.blade.php
@@ -30,35 +30,6 @@
                 <label for="password" class="form-label">Confirm Password</label>
                 <input type="password" class="form-control" id="password" name="password_confirmation">
             </div>
-
-            <div class="mb-3">
-                <label for="User Role" class="form-label">Select UserRole</label>
-                <select name="role" id="role" class="form-control">
-                    <option value="0">--Select Role--</option>
-                    <option value="admin">Admin</option>
-                    <option value="directorate">Directorate</option>
-                    <option value="district">District</option>
-                </select>
-            </div>
-
-            <div class="mb-3" id="districts_select">
-                <label for="District Name" class="form-label">Select District</label>
-                <select name="district" id="" class="form-control">
-                    <option value="0">--Select District--</option>
-                    <option value="Bilaspur">Bilaspur</option>
-                    <option value="Chamba">Chamba</option>
-                    <option value="Hamirpur">Hamirpur</option>
-                    <option value="Kangra">Kangra</option>
-                    <option value="Kinnaur">Kinnaur</option>
-                    <option value="Kullu">Kullu</option>
-                    <option value="Lahul And Spiti">Lahul And Spiti</option>
-                    <option value="Mandi">Mandi</option>
-                    <option value="Shimla">Shimla</option>
-                    <option value="Sirmaur">Sirmaur</option>
-                    <option value="Solan">Solan</option>
-                    <option value="Una">Una</option>
-                </select>
-            </div>
             <button type="submit" class="btn btn-primary">Update</button>
           </form>
     </div>
