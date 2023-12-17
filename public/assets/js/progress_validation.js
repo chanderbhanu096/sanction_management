435b3270844a093a44016c8e2f3322078804e5c1
thakurakhilesh1998 <thakurakhilesh20@gmail.com>
2023-12-17 21:14:30 +0530

    Jquery validation completed

--- public/assets/js/progress_validation.js
+++ public/assets/js/progress_validation.js
@@ -15,6 +15,7 @@ $('document').ready(function()
         let isCompleted=$('#isCompleted').val();
         let uc_file=$('#uc_file')[0];
         let selectedFile=uc_file.files[0];
+        
 
         // Validate Percentage
         if(!isValidPercentage(percentage_san))
@@ -41,7 +42,9 @@ $('document').ready(function()
             $("#isCompleted").next(".error").remove();
         }
        
-        // Validate If UC file is selected
+        if(isCompleted!==-1)
+        {
+              // Validate If UC file is selected
         if(!selectedFile)
         {
             isValid=false;
@@ -64,11 +67,46 @@ $('document').ready(function()
         {
             $("#uc_file").next(".error").remove();
         }
-        return false;
+        }
+        isValid=validateProgressImage();
+        return isValid;
     }
 
     function isValidPercentage(value) {
     var percentageRegex = /^(\d{1,2}(\.\d{1,2})?|100(\.0{1,2})?)$/;
     return percentageRegex.test(value);
   }
+
+  function validateProgressImage()
+  {
+    let images=$('#imageInput')[0];
+    let files=images.files;
+    if(files.length>3)
+    {
+        $("#imageInput").next(".error").remove();
+        $("#imageInput").after("<span class='error'>You can select only 3 images.</span>");
+        return false;
+    }
+    for( let i=0;i<files.length;i++)
+    {
+        let file=files[i]
+        if(file.size>400*400)
+        {
+            $("#imageInput").next(".error").remove();
+            $("#imageInput").after("<span class='error'>Image size should be less than 400Kb.</span>");
+            return false;
+        }
+        let allowedTypes=['image/jpeg', 'image/jpg', 'image/png'];
+
+        if(allowedTypes.indexOf(file.type)===-1)
+        {
+            $("#imageInput").next(".error").remove();
+            $("#imageInput").after("<span class='error'>Images of only jpeg, jpg and png are allowed.</span>");
+            return false; 
+        }
+    }
+    $("#imageInput").next(".error").remove();
+    return true;
+  }
+
 });
